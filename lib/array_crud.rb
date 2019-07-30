1a8d483fe736768bcdaff86ec93e87fe0c4757fe
Tate Shepard <tateshepard@yahoo.com>
2019-07-30 23:23:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    array = ["one", "two", "three", "four"]
+    add_element_to_end_of_array = ["one", "two", "three", "four"]
     element = "blue"
     element << array
 end
