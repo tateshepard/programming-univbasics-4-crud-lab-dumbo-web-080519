46a2bcdf1cd33801957efff8e8cb8a16715a4a63
Tate Shepard <tateshepard@yahoo.com>
2019-07-30 22:26:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array
   add_element_to_end_of_array = ["array", "element"]
-  add_element_to_end_of_array << "value"
+  anew_element = "value"
+  add_element_to_end_of_array << anew_element
   p add_element_to_end_of_array
 end
 
