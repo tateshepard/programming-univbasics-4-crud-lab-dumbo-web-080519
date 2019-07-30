beb597d9c1128db60f98627a92f37326e5470168
Tate Shepard <tateshepard@yahoo.com>
2019-07-30 23:27:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array.unshift(value)
-end
+  add_element_to_start_of_array = []d
 
 def remove_element_from_end_of_array(array)
   
