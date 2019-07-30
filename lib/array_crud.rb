1abeceac72991a7a87d7f1d199eef300c1cdd986
Tate Shepard <tateshepard@yahoo.com>
2019-07-30 22:37:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array
-  add_element_to_end_of_array.push
-  anew_element = "value"
-  add_element_to_end_of_array << anew_element
-  p add_element_to_end_of_array
+  add_element_to_end_of_array.push("value")
 end
 
 def add_element_to_start_of_array(array, element)
