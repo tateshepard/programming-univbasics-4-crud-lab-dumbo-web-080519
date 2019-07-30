a9f106fd29ef3337136f573e77242e3e81eceb02
Tate Shepard <tateshepard@yahoo.com>
2019-07-30 23:58:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -45,6 +45,6 @@ end
 
 def update_element_from_index(array, index_number, element)
   update_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
-  update_element_from_index
+  update_element_from_index.index(2)
 
 end
