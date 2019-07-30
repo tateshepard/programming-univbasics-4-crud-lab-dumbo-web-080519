339dc1de05e728ff53b140a162bfbaa62109e475
Tate Shepard <tateshepard@yahoo.com>
2019-07-30 22:38:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   ["cat", "dog", "fish", 'cat#2']
 end
 
-def add_element_to_end_of_array
+def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array.push("value")
 end
 
